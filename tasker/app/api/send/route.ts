257772b5d5414d6884ecc8d5a4f5bfabc25c3413
s21sd <srivastavasunny359@gmail.com>
2024-09-04 02:23:36 +0530

    some changes

--- tasker/app/api/send/route.ts
+++ tasker/app/api/send/route.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'sunnysrivastava258@gmail.com',
+            from: 'Acme <onboarding@resend.dev>',
             to: ['srivastavasunny359@gmail.com'],
             subject: 'Hello world',
             react: EmailTemplate({ firstName: 'John' }),
